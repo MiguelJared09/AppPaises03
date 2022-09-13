e111fc0fbff1a1e95b3274962dd0cd134d271253
Miguel Jared <70963460+MiguelJared09@users.noreply.github.com>
2022-09-13 03:41:24 -0500

    first commit

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -7,6 +7,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -14,13 +19,16 @@ export class PorPaisComponent {
   termino : string = '';
   hayError: boolean = false;
   paises:Country[]=[];
+  paisesSugeridos:Country[]=[];
+
+  mostrarSugerencias: boolean = false;
   
-  constructor(private paisService:PaisService) { }
+  constructor(private paisService:PaisService) {  }
 
   buscar( termino: string ){
+    this.mostrarSugerencias = false;
     this.hayError=false;
     this.termino = termino;
-    console.log(this.termino);
     
     /* metodo depreciado pero sirve de observable */
     this.paisService.buscarPais(termino)
@@ -29,27 +37,30 @@ export class PorPaisComponent {
       this.paises = paises;
     }, (err) =>{
       this.hayError = true;
-      this.paises = []
+      this.paises = [];
     });
-    /* of(this.paisService.buscarPais(this.termino))
-    .subscribe({
-      next: (paises) =>{
-        paises.subscribe({
-          next: (pais) => {
-            if(pais.status === 404){
-              this.hayError = true
-            } else{
-              console.log('pais');
-            }
-          }
-        })
-      }
-    }) */
   }
 
   sugerencias( termino:string){
+    if(termino == ''){
+      this.mostrarSugerencias = false;
+      this.paises = [];
+      return;  
+    }
     this.hayError = false;
-    // TODO: crear sugerencias 
+    this.termino = termino;
+
+    this.mostrarSugerencias = true;
+    // TODO: crear sugerencias
+
+    this.paisService.buscarPais(termino).subscribe(
+      paises => this.paisesSugeridos = paises.splice(0,4),
+      (err) => this.paisesSugeridos = []
+    );
+  }
+
+  buscarSugerido(termino:string){
+    this.buscar(termino);
   }
 
 }
